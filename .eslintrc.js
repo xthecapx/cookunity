76314c12c8c89624587c30c7f446a23676ee3215
xthecapx@gmail.com <xthecapx@gmail.com>
2023-09-21 16:14:18 -0500

    initial commit with nodejs hello world

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+    ],
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+  };
+  
